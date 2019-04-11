5cb302cc798c57fda5cb4a94c5be8a4feaaf78ca
Prabakaran K <kprabhanew@gmail.com>
2019-04-12 04:12:48 +0530

    Employees Array Created

--- README.md
+++ README.md
@@ -6,27 +6,10 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Node Verion
+## Steps to be followed
 
-Node: `8.12.0`
-
-## Angular CLI Version
-
-Angular CLI: 7.3.1
-Node: 8.12.0
-OS: win32 x64
-
-rxjs                         5.5.12
-typescript                   2.0.2
-webpack                      2.2.0
-
-Step 1 : Install Bootstrap by executing below command 
-`npm install bootstrap@3 --save`
-
-Once Bootstrap is installed, open .angular-cli.json file and specify the path to the Bootstrap stylesheet (bootstrap.min.css) in the styles property as shown below. 
-
-`"styles": [
-  "../node_modules/bootstrap/dist/css/bootstrap.min.css",
-  "styles.css"
-
-]`
+1. Create Employee Model
+2. Create component by run this command `ng g c employees/listEmployees --spec false --flat true`
+3. Create Employees[] arry in `.ts` file
+4. Add Css
+5. Add the `list-employees.component` into `app.component`
---
+++ src/app/employees/list-employees.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {Employee} from '../models/employee.model';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+  employees: Employee[] =  [
+    {
+      "id": 2,
+      "name": "Mary",
+      "gender": "Female",
+      "contactPreference": "Phone",
+      "phoneNumber": 2345978640,
+      "dateOfBirth": new Date('2/11/1979'),
+      "department": "HR",
+      "email" : "abc@gmail.com",
+      "isActive": true,
+      "photoPath": "assets/images/mary.png"
+    },
+    {
+      "id": 3,
+      "name": "John",
+      "gender": "Male",
+      "contactPreference": "Phone",
+      "phoneNumber": 5432978640,
+      "dateOfBirth": new Date('3/11/1980'),
+      "department": "Marketing",
+      "email" : "xyz@gmail.com",
+      "isActive": false,
+      "photoPath": "assets/images/ravi.png"
+    },
+    {
+      "id": 4,
+      "name": "David",
+      "gender": "Female",
+      "contactPreference": "Phone",
+      "phoneNumber": 2345978640,
+      "dateOfBirth": new Date('4/11/1982'),
+      "department": "Sales",
+      "email" : "prq@gmail.com",
+      "isActive": true,
+      "photoPath": "assets/images/ravi.png"
+    }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
